b79c8498ea9a21f779ac505bce902758350644f8
Meho Garanovic <meho_g@live.com>
2024-09-08 00:01:00 +0200

    add link to category

--- resources/views/blogs/all-post.blade.php
+++ resources/views/blogs/all-post.blade.php
@@ -20,6 +20,14 @@
                     </div>
                 @endforelse
             </div>
+            <div class="flex justify-center pt-5">
+                <a href="{{ route('filamentblog.category.post', ['category' => $category->slug]) }}" class="flex items-center justify-center md:gap-x-5 rounded-full bg-slate-100 px-20 py-4 text-sm font-semibold transition-all duration-300 hover:bg-slate-200">
+                    <span>View all</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6" viewBox="0 0 24 24">
+                        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6 18L18 6m0 0H9m9 0v9" />
+                    </svg>
+                </a>
+            </div>
         </div>
     </section>
     @endforeach
