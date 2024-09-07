bb20cae4bf0f26acdd0b287566b7afb37222bc2c
Meho Garanovic <meho_g@live.com>
2024-09-07 17:53:05 +0200

    refactor blog show

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -1,39 +1,36 @@
 <x-blog-layout>
     <section class="pb-16">
-        <div class="container mx-auto px-5" style="max-width: 900px">
+        <div class="container mx-auto px-5" style="max-width: 700px">
             <div class="mb-5 flex gap-x-2 text-sm font-semibold">
-                <a href="{{ route('filamentblog.post.index') }}" class="opacity-60">Home</a>
+                <a href="/" class="opacity-60">Home</a>
                 <span class="opacity-30">/</span>
-                <a href="{{ route('filamentblog.post.all') }}" class="opacity-60">Blog</a>
-                <span class="opacity-30">/</span>
-                <a title="{{ $post->slug }}" href="{{ route('filamentblog.post.show', ['post' => $post->slug]) }}" class="hover:text-primary-600 max-w-2xl truncate font-medium transition-all duration-300">
-                    {{ $post->title }}
-                </a>
+                <a href="{{ route('filamentblog.post.index') }}" class="opacity-60">Blog</a>
             </div>
             <div class="mx-auto mb-20 space-y-10">
                 <div class="grid">
                     <div class="space-y-10">
                         <div>
                             <div class="flex flex-col justify-end">
+                                <h1 class="mb-6 text-4xl font-semibold">
+                                    {{ $post->title }}
+                                </h1>
                                 <div class="mb-6 h-full w-full overflow-hidden rounded bg-slate-200">
-                                    <img class="flex h-full min-h-[400px] items-center justify-center object-cover object-top text-sm text-xl font-semibold text-slate-400 w-full" src="{{ $post->featurePhoto  }}" alt="{{ $post->photo_alt_text }}">
+                                    <img class="flex h-full min-h-[400px] items-center justify-center object-cover object-top text-sm text-xl font-semibold text-slate-400 w-full"
+                                        src="{{ $post->featurePhoto  }}" alt="{{ $post->photo_alt_text }}">
                                 </div>
                                 <div class="mb-6">
-                                    <h1 class="mb-6 text-4xl font-semibold">
-                                        {{ $post->title }}
-                                    </h1>
                                     <p>{{ $post->sub_title }}</p>
                                     <div class="mt-2">
                                         @foreach ($post->categories as $category)
-                                        <a href="{{ route('filamentblog.category.post', ['category' => $category->slug]) }}">
-                                            <span class="bg-primary-200 text-primary-800 mr-2 inline-flex rounded-full px-2 py-1 text-xs font-semibold">{{ $category->name }}
+                                        <a
+                                            href="{{ route('filamentblog.category.post', ['category' => $category->slug]) }}">
+                                            <span
+                                                class="bg-primary-200 text-primary-800 mr-2 inline-flex rounded-full px-2 py-1 text-xs font-semibold">{{
+                                                $category->name }}
                                             </span>
                                         </a>
                                         @endforeach
                                     </div>
-                                </div>
-                                <div class="mb-5 flex items-center justify-between gap-x-3 py-5">
-                                    @include('filament-blog::partials.author', ['post' => $post])
                                 </div>
                                 <div>
                                     <article class="m-auto leading-6">
@@ -41,12 +38,19 @@
                                         {!! tiptap_converter()->asHTML($post->body, toc: false, maxDepth: 3) !!}
                                     </article>
 
+                                    <div class="mb-0 flex items-center justify-between gap-x-3 py-5">
+                                        @include('filament-blog::partials.author', ['post' => $post])
+                                    </div>
+
+                                    <hr>
+
                                     @if($post->tags->count())
                                     <div class="pt-10">
                                         <span class="mb-3 block font-semibold">Tags</span>
                                         <div class="space-x-2 space-y-1">
                                             @foreach ($post->tags as $tag)
-                                            <a href="{{ route('filamentblog.tag.post', ['tag' => $tag->slug]) }}" class="rounded-full border border-slate-300 px-3 py-1 text-sm font-medium font-medium text-black text-slate-600 hover:bg-slate-100">
+                                            <a href="{{ route('filamentblog.tag.post', ['tag' => $tag->slug]) }}"
+                                                class="rounded-full border border-slate-300 px-3 py-1 text-sm font-medium font-medium text-black text-slate-600 hover:bg-slate-100">
                                                 {{ $tag->name }}
                                             </a>
                                             @endforeach
@@ -65,10 +69,12 @@
                                 @foreach($post->comments as $comment)
                                 <article class="pt-4 text-base">
                                     <div class="mb-4 flex items-center gap-4">
-                                        <img class="h-14 w-14 overflow-hidden rounded-full border-4 border-white bg-zinc-300 object-cover text-[0] ring-1 ring-slate-300" src="{{ asset($comment->user->avatar) }}" alt="avatar">
+                                        <img class="h-14 w-14 overflow-hidden rounded-full border-4 border-white bg-zinc-300 object-cover text-[0] ring-1 ring-slate-300"
+                                            src="{{ asset($comment->user->avatar) }}" alt="avatar">
                                         <div>
 
-                                            <span class="block max-w-[150px] overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
+                                            <span
+                                                class="block max-w-[150px] overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
                                                 {{ $comment->user->{config('filamentblog.user.columns.name')} }}
                                             </span>
                                             <span class="block whitespace-nowrap text-sm font-medium text-zinc-600">
@@ -87,44 +93,47 @@
                         @endif
                     </div>
                     <div>
-                        {{-- Ads Section            --}}
-                        {{-- <div--}}
-                        {{-- class="sticky top-24 flex h-[600px] w-[160px] items-center justify-center overflow-hidden rounded bg-slate-200 font-medium text-slate-500/20">--}}
-                        {{-- <span>ADS</span>--}}
-                        {{-- </div>--}}
-                    </div>
+                        {{-- Ads Section --}}
+                        {{-- <div--}} {{--
+                            class="sticky top-24 flex h-[600px] w-[160px] items-center justify-center overflow-hidden rounded bg-slate-200 font-medium text-slate-500/20">
+                            --}}
+                            {{-- <span>ADS</span>--}}
+                            {{-- </div>--}}
                 </div>
             </div>
+        </div>
 
+        <div>
             <div>
-                <div>
-                    <div class="relative mb-6 flex items-center gap-x-8">
-                        <h2 class="whitespace-nowrap text-xl font-semibold">
-                            <span class="text-primary font-bold">#</span> Related Posts
-                        </h2>
-                        <div class="flex w-full items-center">
-                            <span class="h-0.5 w-full rounded-full bg-slate-200"></span>
-                        </div>
-                    </div>
-                    <div class="grid md:grid-cols-3 sm:grid-cols-1 gap-x-12 gap-y-10">
-                        @forelse($post->relatedPosts() as $post)
-                        <x-blog-card :post="$post" />
-                        @empty
-                        <div class="col-span-3">
-                            <p class="text-center text-xl font-semibold text-gray-300">No related posts found.</p>
-                        </div>
-                        @endforelse
-                    </div>
-                    <div class="flex justify-center pt-20">
-                        <a href="{{ route('filamentblog.post.all') }}" class="flex items-center justify-center md:gap-x-5 rounded-full bg-slate-100 px-20 py-4 text-sm font-semibold transition-all duration-300 hover:bg-slate-200">
-                            <span>Show all blogs</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6" viewBox="0 0 24 24">
-                                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6 18L18 6m0 0H9m9 0v9" />
-                            </svg>
-                        </a>
+                <div class="relative mb-6 flex items-center gap-x-8">
+                    <h2 class="whitespace-nowrap text-xl font-semibold">
+                        <span class="text-primary font-bold">#</span> Related Posts
+                    </h2>
+                    <div class="flex w-full items-center">
+                        <span class="h-0.5 w-full rounded-full bg-slate-200"></span>
                     </div>
                 </div>
+                <div class="grid md:grid-cols-3 sm:grid-cols-1 gap-x-12 gap-y-10">
+                    @forelse($post->relatedPosts() as $post)
+                    <x-blog-card :post="$post" />
+                    @empty
+                    <div class="col-span-3">
+                        <p class="text-center text-xl font-semibold text-gray-300">No related posts found.</p>
+                    </div>
+                    @endforelse
+                </div>
+                {{-- <div class="flex justify-center pt-20">
+                    <a href="{{ route('filamentblog.post.all') }}"
+                        class="flex items-center justify-center md:gap-x-5 rounded-full bg-slate-100 px-20 py-4 text-sm font-semibold transition-all duration-300 hover:bg-slate-200">
+                        <span>Show all blogs</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6" viewBox="0 0 24 24">
+                            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="1.5" d="M6 18L18 6m0 0H9m9 0v9" />
+                        </svg>
+                    </a>
+                </div> --}}
             </div>
+        </div>
         </div>
     </section>
     {!! $shareButton?->script_code !!}
